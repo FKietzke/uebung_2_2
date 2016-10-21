dbcfd28c82258c62eea6233346ef71e5eb01e87b
14696 <14696@A101-09.nordakademie.intern>
2016-10-21 11:36:16 +0200

    abgabe

---
+++ src/uebung_2_2/uebung_2_2.java
@@ -0,0 +1,18 @@
+package uebung_2_2;
+
+public class uebung_2_2 {
+	
+	public static void main(String [] args) { 
+		double a = 8;
+		double b = 3;
+				
+		double inDerKlammer = a + b;
+		
+		double loesung1 = Math.pow(inDerKlammer,2); 
+		double loesung2 = Math.pow(a, 2) + 2 * a * b + Math.pow(b,2);
+		
+		System.out.println(loesung1);
+		System.out.println(loesung2);
+}
+	
+}
